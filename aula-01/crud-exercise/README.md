70662adda7c53acf236e2a4fc539c49716742f61
lufecrx <luizfelipecrx@gmail.com>
2024-03-12 21:27:56 -0300

    deletar pastas movidas

--- aula-01/crud-exercise/README.md
+++
@@ -1,2 +0,0 @@
-## Documentação da API
-Para acessar a documentação da API, basta acessar a URL: `http://localhost:8080/swagger-ui/index.html`.
--- aula-01/crud-exercise/src/main/java/br/com/lufecrx/crudexercise/repository/EntityExampleRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.lufecrx.crudexercise.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.lufecrx.crudexercise.model.EntityExampleModel;
-
-public interface EntityExampleRepository extends CrudRepository<EntityExampleModel, Long> {
-    
-}   
